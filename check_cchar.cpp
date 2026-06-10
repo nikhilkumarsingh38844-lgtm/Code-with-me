e98c486c61e7bda1b7aed833bbb9d4f00684d378
Nikhil <nikhilkumarsingh38844@gmail.com>
2026-06-10 22:27:16 +0530

    check for a vowel or consonant using switch-case

---
+++ check_cchar.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter the character:-";
+    cin>>ch;
+    switch (ch)
+    {
+    case 'a':
+        cout<<"Vowel";
+        break;
+    case 'e':
+        cout<<"Vowel";
+        break;
+    case 'i':
+        cout<<"Vowel";
+        break;
+    case 'o':
+        cout<<"Vowel";
+        break;
+    case 'u':
+        cout<<"Vowel";
+        break;
+    case 'A':
+        cout<<"Vowel";
+        break;
+    case 'E':
+        cout<<"Vowel";
+        break;
+    case 'I':
+        cout<<"Vowel";
+        break;
+     case 'O':
+        cout<<"Vowel";
+        break;
+    case 'U':
+        cout<<"Vowel";
+        break;
+    default:
+    cout<<"Consonant";
+     break;  
+    }
+    return 0;
+}
